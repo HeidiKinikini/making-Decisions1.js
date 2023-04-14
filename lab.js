26fca3e99621211bfc3648465325d00441952c2f
Heidi Kinikini <Duckwitz52@gmail.com>
2023-04-14 00:17:27 -0600

    updated last practice problem-md1.js

--- lab.js
+++ lab.js
@@ -147,10 +147,23 @@ console.log(changeMyMind)
 
 ////////// PROBLEM 11 //////////
 
-let z = 5;
+let z = 5; //let z = 6;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
-while (z > 0){
-  z--
-  console.log(z)
-}
+//let loggedCounts = [];
+
+//while(z > 1) {
+ // z--
+ // loggedCounts.push(z)
+ // console.log(loggedCounts)}
+
+  while(z > 0) {
+  let x = z;
+  while (x > 0){
+    console.log(x);
+    x--;
+  }
+  z--;
+  }
+
+
